b190c00024351c3b621349b4f8772b82fd306139
phil-bstorm <philippe.haerens@bstorm.be>
2025-04-07 16:37:22 +0200

    [Correction] Exercices slide 113

---
+++ SqlUtility/Command.cs
@@ -0,0 +1,96 @@
+﻿using Microsoft.Data.SqlClient;
+using System.Data;
+
+namespace SqlUtility
+{
+    public static class Command
+    {
+        public static bool AddSection(SqlConnection conn, int id, string sectionName)
+        {
+            try
+            {
+                using (SqlCommand command = new SqlCommand("AddSection", conn))
+                {
+                    command.CommandType = CommandType.StoredProcedure;
+                    command.Parameters.AddWithValue("@Id", id);
+                    command.Parameters.AddWithValue("@SectionName", 666);
+
+                    command.ExecuteNonQuery();
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                return false;
+            }
+            return true;
+        }
+
+        public static bool AddStudent(SqlConnection conn, string lastname, string firstname, DateTime birthDate, int yearResult, int sectionId)
+        {
+            try
+            {
+                using (SqlCommand command = new SqlCommand("AddStudent", conn))
+                {
+                    command.CommandType = CommandType.StoredProcedure;
+                    command.Parameters.AddWithValue("@Firstname", firstname);
+                    command.Parameters.AddWithValue("@Lastname", lastname);
+                    command.Parameters.AddWithValue("@BirthDate", birthDate);
+                    command.Parameters.AddWithValue("@YearResult", yearResult);
+                    command.Parameters.AddWithValue("@SectionID", sectionId);
+
+
+                    command.ExecuteNonQuery();
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                return false;
+            }
+            return true;
+        }
+
+        public static bool UpdateStudent(SqlConnection conn, int studentId, int sectionId, int yearResult)
+        {
+            try
+            {
+                using (SqlCommand command = new SqlCommand("UpdateStudent", conn))
+                {
+                    command.CommandType = CommandType.StoredProcedure;
+                    command.Parameters.AddWithValue("@StudentId", studentId);
+                    command.Parameters.AddWithValue("@SectionID", sectionId);
+                    command.Parameters.AddWithValue("@YearResult", yearResult);
+
+                    command.ExecuteNonQuery();
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                return false;
+            }
+            return true;
+        }
+
+        public static bool DeleteStudent(SqlConnection conn, int id)
+        {
+            try
+            {
+                using (SqlCommand command = new SqlCommand("DeleteStudent", conn))
+                {
+                    command.CommandType = CommandType.StoredProcedure;
+                    command.Parameters.AddWithValue("@Id", id);
+
+                    command.ExecuteNonQuery();
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                return false;
+            }
+            return true;
+        }
+    }
+}
---
+++ SqlUtility/Connection.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.Data.SqlClient;
+using System.Data;
+
+namespace SqlUtility
+{
+    public static class Connection
+    {
+        /*
+         * Usage:
+         * Count the number of students that have a year result greater than 10
+         * ExecuteScalar(conn, "SELECT COUNT(*) FROM Student WHERE YearResult > @0", 10);
+         */
+        public static T ExecuteScalar<T>(SqlConnection conn, string sql, params object[] args)
+        {
+            using (SqlCommand command = new SqlCommand(sql, conn))
+            {
+                for (int i = 0; i < args.Length; i++)
+                {
+                    command.Parameters.AddWithValue($"@{i}", args[i]);
+                }
+
+                return (T)command.ExecuteScalar();
+            }
+        }
+
+        public static IEnumerable<T> ExecuteReader<T>(SqlConnection conn, string sql, Func<SqlDataReader, T> readerFunc, params object[] args)
+        {
+            using (SqlCommand command = new SqlCommand(sql, conn))
+            {
+                for (int i = 0; i < args.Length; i++)
+                {
+                    command.Parameters.AddWithValue($"@{i}", args[i]);
+                }
+
+                List<T> list = new List<T>();
+                using (SqlDataReader reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        list.Add(readerFunc(reader));
+                    }
+                }
+
+                return list;
+            }
+        }
+
+        public static int ExecuteNonQuery(string sql, SqlConnection conn, params object[] args)
+        {
+            using (SqlCommand command = new SqlCommand(sql, conn))
+            {
+                for (int i = 0; i < args.Length; i++)
+                {
+                    command.Parameters.AddWithValue($"@{i}", args[i]);
+                }
+
+                return command.ExecuteNonQuery();
+            }
+        }
+
+        public static DataTable GetDataTable(SqlConnection conn, string sql, params object[] args)
+        {
+            using (SqlCommand command = new SqlCommand(sql, conn))
+            {
+                for (int i = 0; i < args.Length; i++)
+                {
+                    command.Parameters.AddWithValue($"@{i}", args[i]);
+                }
+
+                SqlDataAdapter adapter = new SqlDataAdapter(command);
+                DataTable table = new DataTable();
+                adapter.Fill(table);
+
+                return table;
+            }
+        }
+    }
+}
